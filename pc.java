5f6cf7432bb00266e4fe45794aea1e7ac76bed5e
Esther <esthermarigomez@gmail.com>
2020-04-23 09:38:05 -0400

    aumentamos comentarios

--- pc.java
+++ pc.java
@@ -2,15 +2,12 @@ public class pc {
     public static void main (String []args ){
         int matriz [][]= new int [5][5];
 
-
-        for (int i=1; i<5;i++){
+        for (int i=1; i<5;i++){//Utilizamos dos fors para mostrar nuestra matriz
             System.out.println();
             for (int j=1; j<5;j++){
                 matriz [i][j]=j*2;
                 System.out.print(matriz[i][j]+" ");
-
             }
-
         }
     }
 }
