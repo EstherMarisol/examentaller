ce32526466bb97ec5d1eee26524ad2f792a45d0e
Esther <esthermarigomez@gmail.com>
2020-04-23 09:41:27 -0400

    volvemos a poner los comentarios y borramos un for

--- pc.java
+++ pc.java
@@ -1,18 +1,13 @@
 public class pc {
     public static void main (String []args ){
         int matriz [][]= new int [5][5];
-
-        for (int i=1; i<5;i++){
-            System.out.println("Esta es nuestra matriz");
+// esta es la ultima modificacion que hacemos
+        for (int i=1; i<5;i++){//Utilizamos dos fors para mostrar nuestra matriz
+            System.out.println();
             for (int j=1; j<5;j++){
                 matriz [i][j]=j*2;
                 System.out.print(matriz[i][j]+" ");
             }
         }
-        for (int i=1; i<5;i++){
-            System.out.println();
-            for (int j=1; j<5;j++){
-                matriz [i][j]=j*2;
-                System.out.print(matriz[i][j]+" ");
     }
 }
