9b22f7e3c823bf85dae1b9219cf84b6bc3095d2a
Esther <esthermarigomez@gmail.com>
2020-04-23 09:30:04 -0400

    Primer commit de pc

---
+++ pc.java
@@ -0,0 +1,16 @@
+public class pc {
+    public static void main (String []args ){
+        int matriz [][]= new int [5][5];
+
+
+        for (int i=1; i<5;i++){
+            System.out.println();
+            for (int j=1; j<5;j++){
+                matriz [i][j]=j*2;
+                System.out.print(matriz[i][j]+" ");
+
+            }
+
+        }
+    }
+}
